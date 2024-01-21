ed8c6ce20cb0bbfe28f4c5eb569f041ec202b8e8
Paul Coral <paul.coral@epfl.ch>
2024-01-21 04:20:10 +0100

    add deploy script

---
+++ deploy-gh-pages.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+git checkout main
+npm install
+ng b --configuration production --aot --base-href=/angular-game-of-life/
+
+git checkout gh-pages
+mv dist/angular-game-of-life/browser/* .
+
+git add .
+git commit
+
